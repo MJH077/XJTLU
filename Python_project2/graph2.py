47b160b3054aeaa8e62d9fc71fa4906ff71b9e0c
jinghanma <jinghanma077@gmail.com>
2026-04-03 20:54:31 +0800

    Delete Python_project2 directory

--- Python_project2/graph2.py
+++
@@ -1,7 +0,0 @@
-from pyecharts.charts import Bar
-from pyecharts.options import LabelOpts
-bar = Bar()
-bar.add_xaxis(["中国", "美国"])
-bar.add_yaxis("GDP", [30, 20], label_opts=LabelOpts(position='right'))
-bar.reversal_axis()  # 反转xy轴
-bar.render("基础柱状图.html")
